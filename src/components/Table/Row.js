f4deb0da74fbeeb63f5ebe27aed87e4a2633e01d
Chris Vita <chris.vita@gmail.com>
2018-02-07 15:50:34 -0800

    Add `Row` subcomponent

---
+++ src/components/Table/Row.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Row = props => {
+  const { firstName, lastName, country, street, city, state, zip, phone } = props;
+  return (
+    <tr>
+      <td>{firstName}</td>
+      <td>{lastName}</td>
+      <td>{country}</td>
+      <td>{street}</td>
+      <td>{city}</td>
+      <td>{state}</td>
+      <td>{zip}</td>
+      <td>{phone}</td>
+    </tr>
+  );
+}
+
+export default Row;
