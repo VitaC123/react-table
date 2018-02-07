384e677486ad9854fbc183872bc1c95927274e83
Chris Vita <chris.vita@gmail.com>
2018-02-07 15:56:51 -0800

    Add core methods to `Table` component

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,30 +1,162 @@
 import React, { Component } from 'react';
+import mockAjaxResponse from '../../MOCK_DATA.json';
+import Dropdown from './Dropdown';
+import Row from './Row';
 import './Table.css';
 
 
 export default class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.sortList = this.sortList.bind(this);
+    this.changeSelection = this.changeSelection.bind(this);
+    this.changePage = this.changePage.bind(this);
+    this.state = {
+      list: [],
+      sortBy: 'last name',
+      itemsPerPage: 10,
+      display: { min: 1, max: 10 }
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      list: this.sortList(mockAjaxResponse, 'last name')
+    });
+  }
+  changeSelection(option) {
+    const { sortBy, list } = this.state;
+    if (typeof option === 'number') {
+      const { min } = this.state.display;
+      return this.setState({
+        itemsPerPage: option,
+        display: { min, max: min + option - 1 }
+      });
+    }
+    if (option === sortBy) {
+      return this.setState({
+        list: list.reverse()
+      });
+    }
+    this.setState({
+      list: this.sortList(this.state.list, option),
+      sortBy: option
+    });
+  }
+  sortList(list, by) {
+    const key = by
+      .replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => index === 0 ? letter.toLowerCase() : letter.toUpperCase())
+      .replace(/\s+/g, '');
+
+    const numerical = (a, b) => (a[key] - b[key]);
+    const alphabetical = (a, b) => {
+      const nameA = a[key].toLowerCase();
+      const nameB = b[key].toLowerCase();
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+    };
+
+    if (by === 'zip') {
+      return list.sort(numerical);
+    }
+    if (by === 'address') {
+      return list.sort((a, b) => {
+        const houseA = a.street.split(' ');
+        const houseB = b.street.split(' ');
+
+        const houseNumA = parseInt(houseA[0], 10);
+        const houseNumB = parseInt(houseB[0], 10);
+        if (houseNumA !== houseNumB) {
+          return houseNumA - houseNumB
+        } else {
+          // compare street names
+          const streetA = houseA[1].toLowerCase();
+          const streetB = houseB[1].toLowerCase();
+          const streetAInt = parseInt(streetA, 10);
+          const streetBInt = parseInt(streetB, 10);
+          // handle comparison if street name contains a number
+          if (streetAInt >= 0 && streetBInt >= 0) return streetAInt - streetBInt;
+          if (streetBInt >= 0) return 1;
+          // handle comparison alphabetically
+          if (streetA < streetB) return -1;
+          if (streetA > streetB) return 1;
+          return 0;
+        }
+      });
+    }
+    return list.sort(alphabetical);
+  }
+  changePage(action) {
+    const { itemsPerPage, list } = this.state;
+    const { min, max } = this.state.display;
+    if (action === 'next') {
+      if (max + itemsPerPage > list.length) {
+        return this.setState({
+          display: { min: list.length - itemsPerPage, max: list.length }
+        });
+      }
+      this.setState({
+        display: { min: min + itemsPerPage, max: max + itemsPerPage }
+      });
+    }
+    if (action === 'previous') {
+      if (min - itemsPerPage < 1) {
+        return this.setState({
+          display: { min: 1, max: itemsPerPage }
+        });
+      }
+      this.setState({
+        display: { min: min - itemsPerPage, max: max - itemsPerPage }
+      });
+    }
+  }
+
   render() {
+    const { list, display, sortBy, itemsPerPage } = this.state;
+    const columns = ['first name', 'last name', 'country', 'address', 'city', 'state', 'zip', 'phone'];
+
+    const tableHeadings = columns.map(column => (
+      <th
+        className={column === 'address' ? 'wide' : ''}
+        onClick={() => this.changeSelection(column)}
+        key={column}
+      >
+        {column}
+      </th>
+    ));
+
+    const rows = list.slice(display.min, display.max).map(customer => (
+      <Row {...customer} key={customer.phone} />
+    ));
+
     return (
       <div>
         <div className='table-header'>
           <section className='left'>
             <h1 className='table-heading'>List of Awesome</h1>
-            <div className='option-group sort-by'>
-              <h2>Sort by:</h2>
-              <button>Last name <i className='fas fa-sort-down' /></button>
-            </div>
+            <Dropdown
+              heading='Sort by'
+              customClass='sort-by'
+              options={columns}
+              handleSelection={this.changeSelection}
+              selection={sortBy}
+            />
           </section>
 
           <section className='right'>
-            <div className='option-group items-per-page'>
-              <h2>items per page:</h2>
-              <button>10 <i className='fas fa-sort-down' /></button>
-            </div>
+            <Dropdown
+              heading='items per page'
+              customClass='items-per-page'
+              options={[5, 10, 25, 50, 75, 100]}
+              handleSelection={this.changeSelection}
+              selection={itemsPerPage}
+            />
+
             <div className='option-group pagination'>
-              <h2>1-10 <span>of</span> 30</h2>
+              <h2>{display.min} - {display.max} <span>of</span> {list.length}</h2>
               <div>
-                <button><i className='fas fa-chevron-left' /></button>
-                <button><i className='fas fa-chevron-right' /></button>
+                <button onClick={() => this.changePage('previous')}><i className='fas fa-chevron-left' /></button>
+                <button onClick={() => this.changePage('next')}><i className='fas fa-chevron-right' /></button>
               </div>
             </div>
           </section>
@@ -34,28 +166,12 @@ export default class Table extends Component {
           <table>
             <thead>
               <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Country</th>
-                <th>Address</th>
-                <th>City</th>
-                <th>State</th>
-                <th>Zip</th>
-                <th>Phone</th>
+                {tableHeadings}
               </tr>
             </thead>
 
             <tbody>
-              <tr>
-                <td>John</td>
-                <td>Snow</td>
-                <td>The North</td>
-                <td>18719 the Wall</td>
-                <td>Winterfell</td>
-                <td>WA</td>
-                <td>98772</td>
-                <td>425-123-4567</td>
-              </tr>
+              {rows}
             </tbody>
           </table>
         </div>
