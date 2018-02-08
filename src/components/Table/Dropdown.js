f03d7e8c98aff77a4d86f0dfff0001e4ab0d98a2
Chris Vita <chris.vita@gmail.com>
2018-02-08 13:03:56 -0800

    Add 'off click' feature
    Closes dropdown panel by clicking anywhere on screen.

--- src/components/Table/Dropdown.js
+++ src/components/Table/Dropdown.js
@@ -4,34 +4,48 @@ import './Dropdown.css';
 export default class Dropdown extends Component {
   constructor(props) {
     super(props);
-    this.toggle = this.toggle.bind(this);
+    this.toggleOpen = this.toggleOpen.bind(this);
+    this.handleClickToClose = this.handleClickToClose.bind(this);
     this.state = {
-      open: false
+      open: false,
+      listenerTarget: document.getElementsByTagName('body')[0]
     };
   }
-  toggle() {
-    this.setState({
-      open: !this.state.open
-    });
+  toggleOpen(e) {
+    const { listenerTarget } = this.state;
+    listenerTarget.addEventListener('click', this.handleClickToClose);
+    this.setState({ open: true });
+  }
+  handleClickToClose(e) {
+    const { className, innerHTML } = e.target;
+    const { listenerTarget } = this.state;
+    if (className === 'validSelection') {
+      const option = innerHTML > 0 ? parseInt(innerHTML, 10) : innerHTML;
+      this.props.handleSelection(option);
+    }
+    setTimeout(() => {  // To handle case where user clicks dropdown button without making a selection
+      listenerTarget.removeEventListener('click', this.handleClickToClose);
+      this.setState({ open: false });
+    }, 0);
   }
   render() {
     const { heading, customClass, options, selection } = this.props;
     const listItems = options.map(option => (
-      <li key={option} onClick={() => { this.props.handleSelection(option); this.toggle(); }}>{option}</li>
+      <li className='validSelection' key={option}>{option}</li>
     ));
 
     return (
       <div>
         <div className={`option-group ${customClass}`}>
           <h2>{heading}:</h2>
-          <button onClick={this.toggle}>
+          <button onClick={this.toggleOpen}>
             {selection} <i className='fas fa-sort-down' />
           </button>
         </div>
 
         {this.state.open && (
           <div className='dropdown-panel'>
-            <ul>{listItems}</ul>
+            <ul className='list-unstyled'>{listItems}</ul>
           </div>)}
       </div>
     );
