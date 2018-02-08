42335e0a96c98f2349d3ac0c1c6febe4f1edd982
Chris Vita <chris.vita@gmail.com>
2018-02-08 14:41:39 -0800

    Cut comparison screenshot

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Main from './components/Main/Main';
-import Spec from './wireframes/UI_UX_Specs_01.png';
 
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        {/* <img
-          style={{
-            width: '100%',
-            position: 'absolute',
-            zIndex: '-1',
-          }}
-          src={Spec}
-          alt='temp spec'
-        /> */}
-
-        <Main />
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>
+);
 
 export default App;
