8689999394d859af0c5850c6bd3b6f159c7602c2
Chris Vita <chris.vita@gmail.com>
2018-02-08 15:09:54 -0800

    Add alphanumeric sorting
    Useful for English zip codes.

--- src/components/Table/sortHelper.js
+++ src/components/Table/sortHelper.js
@@ -1,47 +1,41 @@
 const sortList = (list, orderBy) => {
   const key = toCamelCase(orderBy);
-
-  if (orderBy === 'zip') {
-    return list.sort(sortNumerical(key));
-  }
-
   if (orderBy === 'address') {
-    return list.sort((a, b) => {
-      const houseA = a.street.split(' ');
-      const houseB = b.street.split(' ');
-
-      const houseNumA = parseInt(houseA[0], 10);
-      const houseNumB = parseInt(houseB[0], 10);
-      if (houseNumA !== houseNumB) {
-        return houseNumA - houseNumB;
-      } else {
-        // compare street names
-        const streetA = houseA[1].toLowerCase();
-        const streetB = houseB[1].toLowerCase();
-        const streetAInt = parseInt(streetA, 10);
-        const streetBInt = parseInt(streetB, 10);
-        // handle comparison if street name contains a number
-        if (streetAInt >= 0 && streetBInt >= 0) return streetAInt - streetBInt;
-        if (streetBInt >= 0) return 1;
-        // Otherwise, handle comparison alphabetically
-        if (streetA < streetB) return -1;
-        if (streetA > streetB) return 1;
-        return 0;
-      }
-    });
+    return list.sort(sortStreetAddress());
   }
+  return list.sort(sortAlphanumeric(key));
+};
 
-  return list.sort(sortAlphabetical(key));
-}
-
-const sortNumerical = key => (
-  (a, b) => (a[key] - b[key])
+const sortStreetAddress = (key = 'street') => (
+  (a, b) => {
+    const houseA = a[key].split(' ');
+    const houseB = b[key].split(' ');
+    // compare house numbers
+    const houseNumA = parseInt(houseA[0], 10);
+    const houseNumB = parseInt(houseB[0], 10);
+    if (houseNumA !== houseNumB) {
+      return houseNumA - houseNumB;
+    } else {
+      // compare street names
+      const streetA = houseA[1].toLowerCase();
+      const streetB = houseB[1].toLowerCase();
+      const streetAInt = parseInt(streetA, 10);
+      const streetBInt = parseInt(streetB, 10);
+      // handle comparison if street name contains a number
+      if (streetAInt >= 0 && streetBInt >= 0) return streetAInt - streetBInt;
+      if (streetBInt >= 0) return 1;
+      // Else, handle comparison alphabetically
+      if (streetA < streetB) return -1;
+      if (streetA > streetB) return 1;
+      return 0;
+    }
+  }
 );
 
-const sortAlphabetical = key => (
+const sortAlphanumeric = key => (
   (a, b) => {
-    const nameA = a[key].toLowerCase();
-    const nameB = b[key].toLowerCase();
+    const nameA = a[key].toString().toLowerCase();
+    const nameB = b[key].toString().toLowerCase();
     if (nameA < nameB) return -1;
     if (nameA > nameB) return 1;
     return 0;
